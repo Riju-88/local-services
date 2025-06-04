c5bf1a610fc4bcf1da79be69e0ad4aac760c801b
Riju-88 <rijumistri4@gmail.com>
2025-06-04 23:35:06 +0530

    + Added auth check for the user-providers route.

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -117,9 +117,14 @@
 
                     <flux:menu.separator />
 
-                    <flux:menu.radio.group>
-                        <flux:menu.item :href="route('user-providers', auth()->user()->id)" icon="squares-2x2" wire:navigate>{{ __('Manage Business') }}</flux:menu.item>
-                    </flux:menu.radio.group>
+                    @auth
+    <flux:menu.radio.group>
+        <flux:menu.item :href="route('user-providers', auth()->user()->id)" icon="squares-2x2" wire:navigate>
+            {{ __('Manage Business') }}
+        </flux:menu.item>
+    </flux:menu.radio.group>
+@endauth
+
                     <flux:menu.radio.group>
                         <flux:menu.item :href="route('settings.profile')" icon="cog" wire:navigate>{{ __('Settings') }}</flux:menu.item>
                     </flux:menu.radio.group>
